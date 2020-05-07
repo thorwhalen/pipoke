74359f88038677faf27d611a14f2c50a92ab1937
twhalen <thor@otosense.com>
2020-05-07 19:40:08 -0400

    0.0.3

--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
 from setuptools import setup
+import os
+
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except:
+        return ""
+
+
+ujoin = lambda *args: '/'.join(args)
+
+name = os.path.basename(os.path.dirname(__file__))
+version = '0.0.2'
+root_url = 'https://github.com/thorwhalen/'
 
 setup(
-    name='pipoke',
-    version='0.0.2',
-    url='https://github.com/thorwhalen/pipoke',
+    name=f"{name}",
+    version=f'{version}',
+    url=ujoin(f"{root_url}", f"{name}"),
     license='Apache Software License',
     author='Thor Whalen',
     install_requires=[
@@ -13,6 +29,11 @@ setup(
     ],
     author_email='thorwhalen1@gmail.com',
     description='Utils to acquire stuff from pypi and interrogate it.',
+    keywords=['pip', 'pypi', 'packaging'],
     include_package_data=True,
     platforms='any',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    download_url=ujoin(f'{root_url}', f'archive/v{version}.zip'),
+
 )
